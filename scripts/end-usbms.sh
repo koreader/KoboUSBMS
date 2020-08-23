06ee69dd0d06994a6b1dcd6d02087102a5ce6438
NiLuJe <ninuje@gmail.com>
2020-08-23 21:22:56 +0200

    Oops, final return code should be mount's, not [
    Otherwise, it returns !0 if there is no SD card

--- scripts/end-usbms.sh
+++ scripts/end-usbms.sh
@@ -53,4 +53,6 @@ mount -t vfat -o "${MOUNT_ARGS}" "${PARTITION}" "/mnt/onboard"
 
 # Handle the SD card now (again, not dealing with the dynamic detection nonsense).
 PARTITION="${DISK}1p1"
-[ -e "${PARTITION}" ] && mount -t vfat -o "${MOUNT_ARGS}" "${PARTITION}" "/mnt/sd"
+if [ -e "${PARTITION}" ] ; then
+	mount -t vfat -o "${MOUNT_ARGS}" "${PARTITION}" "/mnt/sd"
+fi
