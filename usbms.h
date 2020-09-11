41569705b61e80a0b43c4372cfc2cb44f07c5fe4
NiLuJe <ninuje@gmail.com>
2020-09-11 18:27:21 +0200

    Use a simpler sysfs path for the battery

--- usbms.h
+++ usbms.h
@@ -87,7 +87,7 @@
 // So far, those have thankfully been set in stone
 #define NTX_KEYS_EVDEV     "/dev/input/event0"
 #define TOUCHPAD_EVDEV     "/dev/input/event1"
-#define BATT_CAP_SYSFS     "/sys/devices/platform/pmic_battery.1/power_supply/mc13892_bat/capacity"
+#define BATT_CAP_SYSFS     "/sys/class/power_supply/mc13892_bat/capacity"
 #define CHARGER_TYPE_SYSFS "/sys/class/power_supply/mc13892_charger/online"
 
 // Internal storage
