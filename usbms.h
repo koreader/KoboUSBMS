0d939e5c8f77eea7f5a8d7191761189b732507e4
NiLuJe <ninuje@gmail.com>
2021-01-26 17:17:50 +0100

    Update to libevdev 1.10.1

--- usbms.h
+++ usbms.h
@@ -78,7 +78,7 @@
 #endif
 
 // Apparently the libevdev version string isn't available anywhere, so, fake it
-#define LIBEVDEV_VERSION "1.10.0"
+#define LIBEVDEV_VERSION "1.10.1"
 
 // Gettext
 #define _(String) gettext(String)
