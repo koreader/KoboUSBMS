fea7926728b75b2e690e458bfe2f82a6971e2f78
NiLuJe <ninuje@gmail.com>
2024-07-07 08:41:24 +0200

    Oops, forgot to add that earlier...

---
+++ openssh/bsd-closefrom.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2004-2005 Todd C. Miller <Todd.Miller@courtesan.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+// NOTE: https://github.com/openssh/openssh-portable/blob/master/openbsd-compat/bsd-closefrom.c
+
+#ifndef _BSD_CLOSEFROM_H
+#define _BSD_CLOSEFROM_H
+
+#include <dirent.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#ifndef OPEN_MAX
+#	define OPEN_MAX 256
+#endif
+
+// Because our target platforms are too old to support either close_range or the glibc closefrom fallback.
+void bsd_closefrom(int lowfd);
+
+#endif    // _BSD_CLOSEFROM_H
