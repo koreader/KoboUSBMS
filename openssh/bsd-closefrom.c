1eae05f5f0c726ed0d06344be622737f8caf3aac
NiLuJe <ninuje@gmail.com>
2024-07-07 08:30:19 +0200

    Rewrite bsd_closefrom, KISS.
    We don't actually need recursion, so scandir will do just fine.

--- openssh/bsd-closefrom.c
+++ openssh/bsd-closefrom.c
@@ -39,11 +39,11 @@ static void
 	}
 }
 
-// Mimic scandir's alphasort
+// We only care about symlinks
 static int
-    fts_natsort(const FTSENT** a, const FTSENT** b)
+    lnk_only(const struct dirent* d)
 {
-	return strverscmp((*a)->fts_name, (*b)->fts_name);
+	return d->d_type == DT_LNK;
 }
 
 void
@@ -57,51 +57,23 @@ void
 	}
 #endif
 
-	// NOTE: Unlike the original openssh/libbsd implementation, we use fts because it caches the whole thing *first*.
-	//       With a readdir loop, we'd risk missing stuff as procfs doesn't guarantee returning the next entry if we delete the current one,
-	//       c.f., the gymnastics glibc's closefrom_fallback implementation has to deal with.
-	//       This issue was reported in libbsd in https://bugs.freedesktop.org/show_bug.cgi?id=85663,
-	//       and upstream libbsd now does something fancier than the openssh copy:
-	//       https://cgit.freedesktop.org/libbsd/tree/src/closefrom.c
-	//       sudo is still using a standard readdir loop: https://github.com/sudo-project/sudo/blob/main/lib/util/closefrom.c
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunknown-pragmas"
-#pragma clang diagnostic ignored "-Wunknown-warning-option"
-#pragma GCC diagnostic ignored "-Wdiscarded-qualifiers"
-#pragma clang diagnostic ignored "-Wincompatible-pointer-types-discards-qualifiers"
-	char* const fdpath[] = { "/proc/self/fd", NULL };
-#pragma GCC diagnostic pop
-	FTS* restrict ftsp;
-	if ((ftsp = fts_open(fdpath, FTS_PHYSICAL | FTS_XDEV, &fts_natsort)) == NULL) {
+	struct dirent** namelist;
+	int             n = scandir("/proc/self/fd", &namelist, &lnk_only, versionsort);
+	if (n == -1) {
 		// Fall back to brute force closure
 		return closefrom_fallback(lowfd);
 	}
-	// Initialize ftsp with as many toplevel entries as possible.
-	FTSENT* restrict chp;
-	chp = fts_children(ftsp, 0);
-	if (chp == NULL) {
-		// No files to traverse! Unlikely in this context...
-		fts_close(ftsp);
-		return closefrom_fallback(lowfd);
-	}
-	FTSENT* restrict p;
-	while ((p = fts_read(ftsp)) != NULL) {
+
+	while (n--) {
 		char* endp;
-		long  fd;
-		switch (p->fts_info) {
-			case FTS_SL:
-				fd = strtol(p->fts_name, &endp, 10);
-				if (p->fts_name != endp && *endp == '\0' && fd >= 0 && fd < INT_MAX && fd >= lowfd &&
-				    fd != ftsp->fts_rfd) {
-					// NOTE: We'll eventually hit a temporary fd caught during the initial walk,
-					//       it was created by fts itself, but is now gone,
-					//       so close will harmlessly fail with EBADF on that one.
-					close((int) fd);
-				}
-				break;
-			default:
-				break;
+		long  fd = strtol(namelist[n]->d_name, &endp, 10);
+		if (namelist[n]->d_name != endp && *endp == '\0' && fd >= 0 && fd < INT_MAX && fd >= lowfd) {
+			// Note that we'll get to iterate over the temporary fd opened by the initial scandir call.
+			// It's gone now, though, so close will just harmlessly fail with EBADF on that one.
+			close((int) fd);
 		}
+
+		free(namelist[n]);
 	}
-	fts_close(ftsp);
+	free(namelist);
 }
